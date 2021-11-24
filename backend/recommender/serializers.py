fce6c0135bcefa31ab6246cc44d09e3333a67b6c
ssumyee <48689213+ssumyee@users.noreply.github.com>
2021-11-24 23:39:39 +0900

    1124 backend edit

--- backend/recommender/serializers.py
+++ backend/recommender/serializers.py
@@ -5,3 +5,4 @@ class RecommenderSerializer(serializers.ModelSerializer):
     class Meta :
         model = Recommender
         fields =('target_user','products','product_tags','curations','curation_tags')
+        
